063fe0f377b352351d5db9666f78e178041a0640
RuotongWu <161455696+RuotongWu@users.noreply.github.com>
2024-03-12 21:56:22 +0800

    Add files via upload
    homework

---
+++ script.js
@@ -0,0 +1,12 @@
+document.write("Hello World!");
+function sayHello(){
+  var response = prompt("What is your name?");
+  alert("Hello " + reponse + ", have a nice day!");
+}
+//sayHello();
+
+function showTopic(){
+    var x = document.getElementById("demo");
+    x.style.fontSize = "25px";
+    x.style.color = "red";
+}
